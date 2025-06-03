9362691bb71ae41b363afd6ff1981e6340af99aa
ItsLuisYourDev <antocede5@gmail.com>
2025-06-03 01:51:26 -0500

    refactor: move API key to environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     footballApi: {
         baseUrl: 'https://v3.football.api-sports.io',
-        apiKey: process.env.FOOTBALL_API_KEY || 'eeac689d711cf31e35f30562dd2bb057',
+        apiKey: process.env.FOOTBALL_API_KEY,
         league: 242,
         season: 2025
     },
